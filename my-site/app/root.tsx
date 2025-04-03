7dde2dd760dd05a2c91547b48a3d4d76c7f57edf
umar-dim <93053307+umar-dim@users.noreply.github.>
2025-04-03 00:04:26 -0500

    updated my favicon

--- my-site/app/root.tsx
+++ my-site/app/root.tsx
@@ -25,20 +25,22 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				{/* <link rel="icon" href="/favicon.png" type="image/x-icon" /> */}
+				<Meta />
+				<Links />
+				<link rel="icon" type="image/png" href="/favicon.png" />
+			</head>
+			<body>
+				{children}
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
 }
 
 export default function App() {
